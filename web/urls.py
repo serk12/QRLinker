175d0c906a71f6327c3d77f8406a484f119f69aa
Alejandro <alejandro.agustin@est.fib.upc.edu>
2016-07-21 20:26:01 +0200

    linked url

--- web/urls.py
+++ web/urls.py
@@ -9,4 +9,8 @@ urlpatterns = [
     url(r'^$', views.index, name='index'),
     # ex: /cam/
     url(r'^cam$', views.cam, name='cam'),
+    # ex: /linked/
+    url(r'^linked$', views.lnked, name='linked'),
+    # ex: /new_token/
+    url(r'^new_token$', views.new_token, name='new_token'),
 ]
--- web/views.py
+++ web/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
 
 def index(request):
     return render(request, 'web/index.html')
 
 def cam(request):
     return render(request, 'web/cam.html')
+
+def linked(request):
+    return render(request, 'web/linked.html')
+
+def new_token(request):
+    return HttpResponse("Dummy token")
